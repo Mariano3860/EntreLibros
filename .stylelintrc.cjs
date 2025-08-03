cfeded3ca4697863a266cbf01081bb58bf367fe1
Mariano3860 <72263339+Mariano3860@users.noreply.github.com>
2025-08-03 17:24:27 +0200

    update stylelint configuration to include recommended SCSS rules and adjust linting scripts

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-standard', 'stylelint-config-recommended-scss'],
+  plugins: [],
   rules: {
     'block-no-empty': true,
     'color-no-invalid-hex': true,
+    'function-url-no-scheme-relative': null,
+    'import-notation': null,
+    'property-no-vendor-prefix': null,
   },
+  ignoreFiles: ['**/node_modules/**', '**/dist/**'],
 }
