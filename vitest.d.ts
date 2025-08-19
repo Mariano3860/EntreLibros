7d5e12714df188a54a8d86115b9d4adeb77684ce
Rojo <rojo-mariano@hotmail.com>
2025-08-19 14:34:56 -0700

    test: refactor showToast mock in RegisterForm tests and update tsconfig include paths

--- vitest.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vitest" />
-import '@testing-library/jest-dom'
-
-declare global {
-  namespace Vi {
-    interface Assertion<T = unknown> extends jest.Matchers<void, T> {}
-  }
-}
