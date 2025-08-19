0f66e4b06bec6919ed85314c57971d1755b85b91
Rojo <rojo-mariano@hotmail.com>
2025-08-18 23:33:31 -0700

    style: adjust hamburger button layout and improve border radius

--- src/api/books/books.service.ts
+++ src/api/books/books.service.ts
@@ -4,6 +4,8 @@ import { RELATIVE_API_ROUTES } from '@/api/routes'
 import { ApiBook } from './books.types'
 
 export const fetchBooks = async (): Promise<ApiBook[]> => {
-  const response = await apiClient.get<ApiBook[]>(RELATIVE_API_ROUTES.BOOKS.LIST)
+  const response = await apiClient.get<ApiBook[]>(
+    RELATIVE_API_ROUTES.BOOKS.LIST
+  )
   return response.data
 }
